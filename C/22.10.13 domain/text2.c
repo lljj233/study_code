7dd8aac4799d7bbeff93e9bca926df93e303b910
lljj233 <963543574@qq.com>
2022-10-16 10:52:50 +0800

    domain

---
+++ C/22.10.13 domain/text2.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+int a = 3;
+int main()
+{
+	int a = 3;
+	printf("a = %d\n", a);
+	{
+		int b = 2;
+		printf("b = %d\n", b);
+	}
+	//printf("b = %d", b);
+}
