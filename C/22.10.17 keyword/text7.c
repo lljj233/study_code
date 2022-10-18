b12baee640fb28e462ae0fe077f7c7408878e785
lljj233 <963543574@qq.com>
2022-10-18 15:27:15 +0800

    初识关键字

---
+++ C/22.10.17 keyword/text7.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+//类型：auto,const,char,enum,int,short,long,float,double
+//     union,struct,sizeof,typedef,signed,unsigned,static,void
+
+//循环：break,continue.do,while,for
+
+//分支：if,else,switch,case,break,default,goto
+
+//函数：return
+
+//extern:声明外部符号
+
+//register:寄存器
+
+//sizeof:计算内存大小
+
+//volatile
+
+
