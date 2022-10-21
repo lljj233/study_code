dcbafddd6a33f8d89a5e53d595ef330ac0ba7697
lljj233 <963543574@qq.com>
2022-10-21 18:41:44 +0800

    牛客网oj简单练习

---
+++ OJ/newcoder/BC_初学者/BC3.c
@@ -0,0 +1,12 @@
+//https://www.nowcoder.com/practice/4712159bbc15417086d40d6c6ff94fee?tpId=107&tqId=33284&rp=1&ru=/ta/beginner-programmers-v1&qru=/ta/beginner-programmers-v1&difficulty=&judgeStatus=&tags=/question-ranking
+
+#include <stdio.h>
+
+int main()
+ {
+    printf("The size of short is %d bytes.\n",sizeof(short));
+    printf("The size of int is %d bytes.\n",sizeof(int));
+    printf("The size of long is %d bytes.\n",sizeof(long));
+    printf("The size of long long is %d bytes.\n",sizeof(long long));
+    return 0;
+}
---
+++ OJ/newcoder/BC_初学者/BC7.c
@@ -0,0 +1,10 @@
+//https://www.nowcoder.com/practice/4ffcc9f206b949ddb057ee0099b34d51?tpId=107&tqId=33288&rp=1&ru=/ta/beginner-programmers-v1&qru=/ta/beginner-programmers-v1&difficulty=&judgeStatus=&tags=/question-ranking
+
+#include<stdio.h>
+
+int main(void)
+{
+int a=1234;
+printf("%#o %#X\n",a,a);
+return 0;
+}
