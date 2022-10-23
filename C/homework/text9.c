eb69e993b2fbb7a3a217b5746b542d88f07cd843
lljj233 <963543574@qq.com>
2022-10-23 21:58:18 +0800

    练习

--- C/homework/text9.c
+++ C/homework/text9.c
@@ -145,3 +145,24 @@
 //    printf("\n");
 //    return 0;
 //}
+
+
+//求输出
+//int func(int a)
+//{
+//    int b;
+//    switch (a)
+//    {
+//    case 1: b = 30;
+//    case 2: b = 20;
+//    case 3: b = 16;
+//    default: b = 0;
+//    }
+//    return b;
+//}
+//int main()
+//{
+//    int m = func(1);
+//    printf("%d", m);
+//    return 0;
+//}
