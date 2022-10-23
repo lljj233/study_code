6cbc7e2582b344837da6be05ea8cbba01d1f1c01
lljj233 <963543574@qq.com>
2022-10-23 18:31:28 +0800

    初始结构体

---
+++ C/22.10.21 structure/text10.c
@@ -0,0 +1,29 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <stdio.h>
+
+//struct STU
+//{
+//	char name[20];
+//	char sex[5];
+//	int age;
+//};
+//
+//void pri(struct STU* p)
+//{
+//	printf("%s %s %d\n",(*p).name,(*p).sex,(*p).age);
+//	printf("%s %s %d\n",p->name,p->sex,p->age);
+//
+//}
+//
+//int main()
+//{
+//	struct STU s1 = {"zhangsan","男",18};
+//	pri(&s1);
+//
+//	printf("%s\n", s1.name);
+//	printf("%s\n", s1.sex);
+//	printf("%d\n", s1.age);
+//
+//	return 0;
+//}
--- C/njupt_c/练习/练习/源.c
+++ C/njupt_c/练习/练习/源.c
@@ -1419,20 +1419,18 @@ int main()
 
 
 
-//#include<stdio.h>
-//int main()
-//{
-//    int i = 0;
-//    int j = 0;
-//    for (i = 100; i < 200; i++)
-//    {
-//        for (j = 2; j < i; j++)
-//        {
-//            if (i % j == 0)
-//            {
-//
-//            }
-//        }
-//    }
-//    return 0;
-//}
+#include<stdio.h>
+int main()
+{
+    int i, j;
+    for (i = 100; i <= 200; i++)
+    {
+        for (j = 2; j < i; j++)
+            if (i % j == 0)
+                break;
+        if (i >= j)
+            printf("%d ", i);
+    }
+    printf("\n");
+    return 0;
+}
