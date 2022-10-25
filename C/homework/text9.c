218c1575e13bcf0a2cc80922a55ff9f7cb8a51f4
lljj233 <963543574@qq.com>
2022-10-25 10:54:11 +0800

    课后练习题

--- C/homework/text9.c
+++ C/homework/text9.c
@@ -203,3 +203,43 @@
 //	printf("%d\n", a);
 //	return 0;
 //}
+
+
+
+//输出9*9乘法口诀表
+//#include <stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int j = 0;
+//	for (i = 1; i < 10; i++)
+//	{
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%d	", j, i, i * j);
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+
+//求十个数的最大值
+//#include<stdio.h>
+//int main()
+//{
+//	int max = 0;
+//	int i = 0;
+//	int num = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		scanf("%d", &num);
+//		if (num > max)
+//		{
+//			max = num;
+//		}
+//	}
+//	printf("最大值为:%d", max);
+//	return 0;
+//}
