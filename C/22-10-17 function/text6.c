ee5b6396b1e943a12407cc688576359ebb6194ae
lljj233 <963543574@qq.com>
2022-10-31 17:35:32 +0800

    strcpy函数

--- C/22-10-17 function/text6.c
+++ C/22-10-17 function/text6.c
@@ -2,21 +2,40 @@
 
 #include <stdio.h>
  
-int add(int a, int b)
-{
-	int c = 0;
-	c = a + b;
-	return c;
-}
+//int add(int a, int b)
+//{
+//	int c = 0;
+//	c = a + b;
+//	return c;
+//}
+//
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum1 = 0;
+//	printf("inut two nums\n");
+//	scanf("%d %d", &num1, &num2);
+//	sum1 = add(num1, num2);
+//	printf("sum=%d\n", sum1);
+//	return 0;
+//}
 
-int main()
-{
-	int num1 = 0;
-	int num2 = 0;
-	int sum1 = 0;
-	printf("inut two nums\n");
-	scanf("%d %d", &num1, &num2);
-	sum1 = add(num1, num2);
-	printf("sum=%d\n", sum1);
-	return 0;
-}
+
+//strcpy函数  拷贝字符串函数
+//#include<stdio.h>
+//#include<string.h>
+//int main()
+//{
+//	char arr1[20] = { 0 };
+//	char arr2[] = "Hello World";
+//
+//	strcpy(arr1, arr2);
+//	printf("%s\n", arr1);
+//
+//	char* ret = strcpy(arr1, arr2);
+//	printf("%s\n", ret);
+//	return 0;
+//}
+
+
