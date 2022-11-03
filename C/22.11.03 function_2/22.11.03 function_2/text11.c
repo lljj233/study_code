d20e8624f4e81093cfcb7e2e762eca04b801b5f8
LLJJ233 <963543574@qq.com>
2022-11-03 15:46:34 +0800

    递归将参数字符串中的字符反向排列

--- C/22.11.03 function_2/22.11.03 function_2/text11.c
+++ C/22.11.03 function_2/22.11.03 function_2/text11.c
@@ -95,3 +95,25 @@
 //	printf("%d", ret);
 //	return 0;
 //}
+
+
+//编写一个函数 reverse_string(char * string)（递归实现）
+//实现：将参数字符串中的字符反向排列，不是逆序打印。
+//要求：不能使用C函数库中的字符串操作函数。
+//#include<stdio.h>
+//void reverse_string(char* string)
+//{
+//	if (*string != '\0')
+//	{
+//		string++;
+//		reverse_string(string);
+//		printf("%c", *(string-1));
+//	}
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	printf("%s\n", arr);
+//	reverse_string(arr);
+//	return 0;
+//}
