e3384393ab74fcc4b2ee71481baf37e659075b07
LLJJ233 <963543574@qq.com>
2022-11-05 19:19:06 +0800

    利用递归计算n的k次方

--- C/22.11.03 function_2/22.11.03 function_2/text11.c
+++ C/22.11.03 function_2/22.11.03 function_2/text11.c
@@ -117,3 +117,25 @@
 //	reverse_string(arr);
 //	return 0;
 //}
+
+
+//利用递归计算n的k次方
+#include<stdio.h>
+double Pow(int n, int k)
+{
+	if (k == 0)
+		return 1.0;
+	else if (k > 0)
+		return n * Pow(n, k - 1);
+	else
+		return 1.0 / Pow(n, -k);
+}
+int main()
+{
+	int n = 0;
+	int k = 0;
+	scanf("%d %d", &n, &k);
+	double ret = Pow(n, k);
+	printf("%lf\n", ret);
+	return 0;
+}
