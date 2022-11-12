0f2e7339bf494e5432501f42e16147e28576792e
LLJJ233 <963543574@qq.com>
2022-11-12 18:36:54 +0800

    练习

--- C/practice/practice/practice.c
+++ C/practice/practice/practice.c
@@ -489,51 +489,65 @@
 //}
 
 
-#include<stdio.h>
-#define ROW 3
-#define COL 3
-void Init_board(char board[ROW][COL], int row, int col)
-{
-	int i = 0;
-	for (i = 0; i < row; i++)
-	{
-		int j = 0;
-		for (j = 0; j < col; j++)
-		{
-			board[i][j] = ' ';
-		}
-	}
-}
-void Print_board(char board[ROW][COL], int row, int col)
-{
-	int i = 0;
-	for (i = 0; i < row; i++)
-	{
-		int j = 0;
-		for (j = 0; j < col; j++)
-		{
-			printf(" %c ", board[i][j]);
-			if(j<col-1)
-				printf("|");
-		}
-		printf("\n");
-		if (i < row-1)
-		{
-			for (j = 0; j < col; j++)
-			{
-				printf("---");
-				if (j < col - 1)
-					printf("|");
-			}
-		}
-		printf("\n");
-	}
-}
-	
-int main()
-{
-	char board[ROW][COL];
-	Init_board(board, ROW, COL);
-	Print_board(board, ROW, COL);
-	return 0;
-}
+//#include<stdio.h>
+//#define ROW 3
+//#define COL 3
+//void Init_board(char board[ROW][COL], int row, int col)
+//{
+//	int i = 0;
+//	for (i = 0; i < row; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < col; j++)
+//		{
+//			board[i][j] = ' ';
+//		}
+//	}
+//}
+//void Print_board(char board[ROW][COL], int row, int col)
+//{
+//	int i = 0;
+//	for (i = 0; i < row; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < col; j++)
+//		{
+//			printf(" %c ", board[i][j]);
+//			if(j<col-1)
+//				printf("|");
+//		}
+//		printf("\n");
+//		if (i < row-1)
+//		{
+//			for (j = 0; j < col; j++)
+//			{
+//				printf("---");
+//				if (j < col - 1)
+//					printf("|");
+//			}
+//		}
+//		printf("\n");
+//	}
+//}
+//	
+//int main()
+//{
+//	char board[ROW][COL];
+//	Init_board(board, ROW, COL);
+//	Print_board(board, ROW, COL);
+//	return 0;
+//}
+
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0, b = 0;
+//	scanf("%d %d", &a, &b);
+//	printf("%d %d\n", a, b);
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("%d %d\n", a, b);
+//	return 0;
+//}
