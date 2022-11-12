5b005586e3c88b66d2019cf01747b5738614f1c1
LLJJ233 <963543574@qq.com>
2022-11-12 19:29:23 +0800

    作业练习

--- C/byte homework/byte homework/text.c
+++ C/byte homework/byte homework/text.c
@@ -795,3 +795,72 @@
 //	}
 //	return 0;
 //}
+
+
+
+//输入一个整数 n ，输出该数32位二进制表示中1的个数。其中负数用补码表示。
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	int count = 0;
+//	int i = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if (((a >> i) & 1) == 1)
+//			count++;
+//	}
+//	printf("%d", count);
+//	return 0;
+//}
+
+
+//获取一个整数二进制序列中所有的偶数位和奇数位，分别打印出二进制序列
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	for (int i = 31; i >= 1; i -= 2)
+//	{
+//		printf("%d ", (a >> i) & 1);
+//	}
+//	printf("\n");
+//	for (int i = 30; i >= 0; i -= 2)
+//	{
+//		printf("%d ", (a >> i) & 1);
+//	}
+//	printf("\n");
+//	return 0;
+//}
+
+
+//编程实现：两个int（32位）整数m和n的二进制表达中，有多少个位(bit)不同？ 
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0, b = 0;
+//	scanf("%d %d", &a, &b);
+//	int c = a ^ b;
+//	int count = 0;
+//	int i = 0;
+//	for (i = 0; i < 32; i++)
+//		if (((c >> i) & 1) == 1)
+//			count++;
+//	printf("%d", count);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 0, b = 0;
+//	scanf("%d %d", &a, &b);
+//	int count = 0;
+//	int i = 0;
+//	for (i = 0; i < 32; i++)
+//		if (((a >> i) & 1) != ((b >> i) & 1))
+//			count++;
+//	printf("%d", count);
+//	return 0;
+//}
