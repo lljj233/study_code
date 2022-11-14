3dfd493a25510762925b29851cef92206c54e477
LLJJ233 <963543574@qq.com>
2022-11-14 09:18:35 +0800

    错题调试

--- C/practice/practice/practice.c
+++ C/practice/practice/practice.c
@@ -680,3 +680,19 @@
 //		Sleep(36);//Sleep函数:执行挂起一段时间，也就是等待一段时间在继续执行
 //	}
 //}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a, b, c;
+//	a = 5;
+//	c = ++a;
+//	b = ++c;
+//	c++;
+//	++a;
+//	a++;
+//	b += a++ + c;
+//	printf("a = %d b = %d c = %d\n:", a, b, c);
+//	return 0;
+//}
