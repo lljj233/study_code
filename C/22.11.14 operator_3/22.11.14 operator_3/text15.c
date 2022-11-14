c2d26e5e116df2cbe8601c0ec56f18bf3bf90a28
LLJJ233 <963543574@qq.com>
2022-11-14 19:07:58 +0800

    自增自减

---
+++ C/22.11.14 operator_3/22.11.14 operator_3/text15.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include<stdio.h>
+
+//自增自减
+
+//int main()
+//{
+//	int a = 10;
+//	int b = a++;//后置++：先使用后++
+//	//相当于int b = a;a = a + 1;
+//	printf("%d\n", a);//11
+//	printf("%d\n", b);//10
+//
+//	int c = 10;
+//	int d = ++c;//前置++：先++后使用
+//	//相当于a = a + 1;b = a;
+//	printf("%d\n", c);//11
+//	printf("%d\n", d);//11
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 10;
+//	printf("%d\n", a++);//10
+//	printf("%d\n", a);//11
+//
+//	int b = 10;
+//	printf("%d\n", ++b);//11
+//	printf("%d\n", b);//11
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 10;
+//	printf("%d\n", a--);//10
+//	printf("%d\n", a);//9
+//
+//	int b = 10;
+//	printf("%d\n", --b);//9
+//	printf("%d\n", b);//9
+//	return 0;
+//}
+
+// ++ -- 是有副作用的(改变自身)
+//int main()
+//{
+//	int a = 10;
+//	int b = ++a;//a=11 b=11
+//
+//	int a = 10;
+//	int b = a + 1;//a=10 b=11
+//	return 0;
+//}
