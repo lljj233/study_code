33478a2ce7747fe4fdd5e8b10f0c44d4706f1741
LLJJ233 <963543574@qq.com>
2022-11-20 09:30:37 +0800

    指针

---
+++ C/22.11.19 pointer_1/22.11.19 pointer_1/text16.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//指针
+//1.指针是内存中一个最小单元的编号，也就是地址
+//2.平时的指针指的是指针变量，用来存放内存地址
+
+#include<stdio.h>
+
+
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a;//取a的地址
+//	//int占四个字节，取出的是第一个字节的地址（较小的地址）
+//
+//	printf("%p\n", &a);
+//	printf("%p\n", pa);
+//	return 0;
+//}
