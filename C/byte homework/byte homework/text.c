424c80adb34e1165a9e1d4a38472f1e85433ef6b
LLJJ233 <963543574@qq.com>
2022-11-23 14:18:48 +0800

    判断两个矩阵是否相等

--- C/byte homework/byte homework/text.c
+++ C/byte homework/byte homework/text.c
@@ -1221,3 +1221,48 @@
 //		printf("NO");
 //	return 0;
 //}
+
+
+
+//判断两个矩阵是否相等
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	int m = 0;
+//	scanf("%d %d", &n, &m);
+//	int arr1[10][10] = { 0 };
+//	int arr2[10][10] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		for (j = 0; j < m; j++)
+//		{
+//			scanf("%d", &arr1[i][j]);
+//		}
+//	}
+//	for (i = 0; i < n; i++)
+//	{
+//		for (j = 0; j < m; j++)
+//		{
+//			scanf("%d", &arr2[i][j]);
+//		}
+//	}
+//	int flag = 1;
+//	for (i = 0; i < n; i++)
+//	{
+//		if (flag = 0)
+//			break;
+//		for (j = 0; j < m; j++)
+//		{
+//			if(arr1[i][j]!=arr2[i][j]);
+//			flag = 0;
+//		}
+//	}
+//	if (flag == 0)
+//		printf("No");
+//	else
+//		printf("Yes");
+//	return 0;
+//}
