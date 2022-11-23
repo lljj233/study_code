a09bbec0d1661993815604bb849902a8ca03b6b1
LLJJ233 <963543574@qq.com>
2022-11-23 13:53:30 +0800

    判断一个n阶方矩是否为上三角矩阵

--- C/byte homework/byte homework/text.c
+++ C/byte homework/byte homework/text.c
@@ -1185,3 +1185,39 @@
 //	get_water(money);
 //	return 0;
 //}
+
+
+
+//判断一个n阶方矩是否为上三角矩阵
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int arr[10][10] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < n; i++)
+//	{
+//		for (j = 0; j < n; j++)
+//		{
+//			scanf("%d", &arr[i][j]);
+//		}
+//	}
+//	int flag = 0;
+//	for (i = 1; i < n; i++)
+//	{
+//		if (flag == 1)
+//			break;
+//		for (j = 0; j < i; j++)
+//		{
+//			if (arr[i][j] != 0)
+//				flag = 1;
+//		}
+//	}
+//	if (flag == 0)
+//		printf("YES");
+//	else
+//		printf("NO");
+//	return 0;
+//}
