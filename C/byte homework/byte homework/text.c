61707d4413b66f45458f60fa7604d75b522d14ce
LLJJ233 <963543574@qq.com>
2022-11-24 08:55:29 +0800

    打印菱形

--- C/byte homework/byte homework/text.c
+++ C/byte homework/byte homework/text.c
@@ -1116,6 +1116,42 @@
 //	return 0;
 //}
 
+//#include<stdio.h>
+//int main()
+//{
+//	int line = 0;
+//	scanf("%d", &line);
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < line; i++)
+//	{
+//		for (j = 0; j < line-1-i; j++)
+//		{
+//			printf(" ");
+//		}
+//		for (j = 0; j < 2*i+1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	for (i = 0; i < line-1; i++)
+//	{
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf(" ");
+//		}
+//		for (j = 0; j < 2*(line-1-i)-1; j++)
+//		{
+//			printf("*");
+//		}
+//		printf("\n");
+//	}
+//	return 0;
+//}
+
+
+
 
 //求出0～100000之间的所有“水仙花数”并输出。
 //#include<stdio.h>
