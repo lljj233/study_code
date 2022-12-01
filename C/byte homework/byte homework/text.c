9ea2f1bd90a1fc75a64359f273843409ea36548b
LLJJ233 <963543574@qq.com>
2022-12-01 22:26:01 +0800

    实现将一句话的单词进行逆序

--- C/byte homework/byte homework/text.c
+++ C/byte homework/byte homework/text.c
@@ -1670,6 +1670,41 @@
 //	return 0;
 //}
 
+//#include<stdio.h>
+//#include<string.h>
+//#include<assert.h>
+//void reverse(char* left, char* right)
+//{
+//	assert(left && right);
+//	while (left < right)
+//	{
+//		char tmp = *left;
+//		*left = *right;
+//		*right = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//int main()
+//{
+//	char arr[100] = { 0 };
+//	gets(arr);
+//	int len = strlen(arr);
+//	reverse(arr, arr + len - 1);
+//	char* cur = arr;
+//	while (*cur)
+//	{
+//		char* start = cur;
+//		while (*cur != ' ' && *cur != '\0')
+//		{
+//			cur++;
+//		}
+//		reverse(start, cur - 1);
+//		if (*cur != '\0')
+//			cur++;
+//	}
+//	printf("%s", arr);
+//}
 
 
 //求两个数的最小公倍数
