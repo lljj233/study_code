267470bcace99e7849ea451be458c7a281abb744
LLJJ233 <963543574@qq.com>
2022-12-03 15:17:03 +0800

    输出成绩最高的前五个

--- C/practice/practice/practice.c
+++ C/practice/practice/practice.c
@@ -1008,3 +1008,35 @@
 //    }
 //    return 0;
 //}
+
+
+//#include <stdio.h>
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int arr[50] = { 0 };
+//    int i = 0;
+//    for (i = 0; i < n; i++)
+//    {
+//        scanf("%d", &arr[i]);
+//    }
+//    int j = 0;
+//    for (j = 0; j < n - 1; j++)
+//    {
+//        for (i = 0; i < n - j -1; i++)
+//        {
+//            if (arr[i] < arr[i + 1])
+//            {
+//                int tmp = arr[i];
+//                arr[i] = arr[i + 1];
+//                arr[i + 1] = tmp;
+//            }
+//        }
+//    }
+//    for (i = 0; i < 5; i++)
+//    {
+//        printf("%d ", arr[i]);
+//    }
+//    return 0;
+//}
