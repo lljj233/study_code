559841ea753b6c6ac9ceef2a714e8c31a75851de
LLJJ233 <963543574@qq.com>
2022-12-09 22:15:05 +0800

    牛客网oj

---
+++ OJ/newcoder/BC_初学者/BC132.c
@@ -0,0 +1,10 @@
+//https://www.nowcoder.com/practice/4ef07b4f80fc4027a4b90e2245f8f637?tpId=107&tags=&title=&difficulty=1&judgeStatus=0&rp=1&sourceUrl=#include <stdio.h>
+
+
+int main()
+ {
+    double a,b,c,d;
+    scanf("%lf %lf %lf %lf",&a,&b,&c,&d);
+    printf("%.1lf",a*0.2+b*0.1+c*0.2+d*0.5);
+    return 0;
+}
---
+++ OJ/newcoder/BC_初学者/BC133.c
@@ -0,0 +1,15 @@
+//https://www.nowcoder.com/practice/659248911f4f4a60bcc7e0892f91c268?tpId=107&rp=1&ru=%2Fta%2Fbeginner-programmers-v1&qru=%2Fta%2Fbeginner-programmers-v1&difficulty=1&judgeStatus=&tags=&title=&sourceUrl=&gioEnter=menu
+
+#include <stdio.h>
+
+int main() 
+{
+    int n;
+    scanf("%d",&n);
+    int i=0;
+    for(i=0;i<n;i++)
+    {
+        printf("Happy new year!Good luck!\n");
+    }
+    return 0;
+}
