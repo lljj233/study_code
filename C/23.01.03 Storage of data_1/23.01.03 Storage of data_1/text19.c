a637e4fb314937f222e77a4741ce908740c55e09
LLJJ233 <963543574@qq.com>
2023-01-03 21:24:16 +0800

    写一个代码判断是大端存储还是小端存储

--- C/23.01.03 Storage of data_1/23.01.03 Storage of data_1/text19.c
+++ C/23.01.03 Storage of data_1/23.01.03 Storage of data_1/text19.c
@@ -29,8 +29,61 @@
 //          高位             低位  
 //大端存储（大端字节序存储）：11 22 33 44 - 高位字节数据放在低地址处
 //小端存储（小端字节序存储）：44 33 22 11 - 低位字节数据放在低地址处
-int main()
-{
-	int a = 0x11223344;
-	return 0;
-}
+//int main()
+//{
+//	int a = 0x11223344;
+//	return 0;
+//}
+
+
+
+//通过代码判断是大端存储还是小端存储
+
+//int main()
+//{
+//	int a = 1;
+//	char* p = (char*)&a;
+//	if (*p == 1)
+//	{
+//		printf("小端存储");
+//	}
+//	else
+//	{
+//		printf("大端存储");
+//	}
+//	return 0;
+//}
+
+//int check_sys()//如果是小端存储返回1，大端存储返回0
+//{
+//	int a = 1;
+//	char* p = (char*)&a;
+//	if (*p == 1)
+//		return 1;
+//	else
+//		return 0;
+//}
+//int main()
+//{
+//	int ret = check_sys();
+//	if(ret==1)
+//		printf("小端存储");
+//	else
+//		printf("大端存储");
+//	return 0;
+//}
+
+//int check_sys()//如果是小端存储返回1，大端存储返回0
+//{
+//	int a = 1;
+//	return *(char*)&a;
+//}
+//int main()
+//{
+//	int ret = check_sys();
+//	if (ret == 1)
+//		printf("小端存储");
+//	else
+//		printf("大端存储");
+//	return 0;
+//}
