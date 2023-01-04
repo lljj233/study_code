a8278cc94e648f502fdd346f18705645b41a2d72
LLJJ233 <963543574@qq.com>
2023-01-04 14:53:24 +0800

    字符指针经典习题

--- C/23.01.04 pointer_3/23.01.04 pointer_3/text20.c
+++ C/23.01.04 pointer_3/23.01.04 pointer_3/text20.c
@@ -12,3 +12,25 @@
 //	
 //	return 0;
 //}
+
+
+//int main()
+//{
+//	char str1[] = "hello";
+//	char str2[] = "hello";
+//
+//	const char* str3 = "hello";
+//	const char* str4 = "hello";
+//
+//	if (str1 == str2)
+//		printf("str1 and str2 are same\n");
+//	else
+//		printf("str1 and str2 are not same\n");
+//
+//	if(str3==str4)
+//		printf("str3 and str4 are same\n");
+//	else
+//		printf("str3and str4 are not same\n");
+//
+//	return 0;
+//}
