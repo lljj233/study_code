5770db0fe30886802912e3fcea30abb0a2bf2b0d
LLJJ233 <963543574@qq.com>
2023-01-04 14:46:11 +0800

    字符指针变量

---
+++ C/23.01.04 pointer_3/23.01.04 pointer_3/text20.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//×Ö·ûÖ¸Õë
+//int main()
+//{
+//	char ch = 'w';
+//	char* pc = &ch;
+//
+//	const char* p = "abcdef";
+//	printf("%s\n", p);
+//	
+//	return 0;
+//}
