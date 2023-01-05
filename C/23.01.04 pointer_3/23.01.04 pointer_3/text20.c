f6d550889e0f83887b114a3774a76c9fa1f2e748
LLJJ233 <963543574@qq.com>
2023-01-05 11:46:53 +0800

    一级指针传参

--- C/23.01.04 pointer_3/23.01.04 pointer_3/text20.c
+++ C/23.01.04 pointer_3/23.01.04 pointer_3/text20.c
@@ -200,3 +200,25 @@
 //	int arr[3][5] = { 0 };
 //	test(arr);
 //}
+
+
+
+//一级指针传参
+//#include <stdio.h>
+//void print(int* p, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d\n", *(p + i));
+//	}
+//}
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9 };
+//	int* p = arr;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//一级指针p，传给函数
+//	print(p, sz);
+//	return 0;
+//}
