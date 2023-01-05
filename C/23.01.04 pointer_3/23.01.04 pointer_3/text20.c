c9205d753a0f65871c5d8670bdf5fe417297b112
LLJJ233 <963543574@qq.com>
2023-01-05 11:31:07 +0800

    二维数组传参

--- C/23.01.04 pointer_3/23.01.04 pointer_3/text20.c
+++ C/23.01.04 pointer_3/23.01.04 pointer_3/text20.c
@@ -174,3 +174,29 @@
 //	test(arr);
 //	test2(arr2);
 //}
+
+
+
+//二维数组传参
+//void test(int arr[3][5])
+//{}
+///*void test(int arr[][])//错误，列不能省略
+//{}*/
+//void test(int arr[][5])
+//{}
+////总结：二维数组传参，函数形参的设计只能省略第一个[]的数字。
+////因为对一个二维数组，可以不知道有多少行，但是必须知道一行多少元素。
+////这样才方便运算。
+///*void test(int* arr)//错误
+//{}*/
+///*void test(int* arr[5])//错误
+//{}*/
+//void test(int(*arr)[5])
+//{}
+///*void test(int** arr)//错误
+//{}*/
+//int main()
+//{
+//	int arr[3][5] = { 0 };
+//	test(arr);
+//}
