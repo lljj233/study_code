b07528c530eca4417fd62129b1c29ca4c64b1c39
LLJJ233 <963543574@qq.com>
2023-01-07 15:31:25 +0800

    几种指针回顾

---
+++ C/23.01.05 pointer_4/23.01.05 pointer_4/text21.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+
+//my_strlen(const char* str)
+//{
+//	return 0;
+//}
+//int main()
+//{
+//	//指针数组
+//	int arr[10] = { 0 };
+//	int(*p)[10] = &arr;
+//
+//	//数组指针
+//	char* ch[5];
+//	
+//	//函数指针
+//	int (*pf)(const char*)=&my_strlen;
+//}
