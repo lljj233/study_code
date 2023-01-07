e5b7c8abff2b6a87228542c371c0c5b697ed0dac
LLJJ233 <963543574@qq.com>
2023-01-07 16:43:34 +0800

    回调函数

--- C/23.01.05 pointer_4/23.01.05 pointer_4/text21.c
+++ C/23.01.05 pointer_4/23.01.05 pointer_4/text21.c
@@ -103,3 +103,86 @@
 //	int(*(*ppf[5]))(int, int) = &pf;//指向函数指针数组的指针
 //	return 0;
 //}
+
+
+
+//回调函数
+//回调函数就是一个通过函数指针调用的函数。如果你把函数的指针（地址）作为参数传递给另一个
+//函数，当这个指针被用来调用其所指向的函数时，我们就说这是回调函数。回调函数不是由该函数
+//的实现方直接调用，而是在特定的事件或条件发生时由另外的一方调用的，用于对该事件或条件进
+//行响应。
+//void menu()
+//{
+//	printf("*****************\n");
+//	printf("****  1.add  ****\n");
+//	printf("****  2.sub  ****\n");
+//	printf("****  3.mul  ****\n");
+//	printf("****  4.div  ****\n");
+//	printf("***** 0.exit ****\n");
+//	printf("*****************\n");
+//}
+//
+//int Add(int x, int y)
+//{
+//	return x + y;
+//}
+//
+//int Sub(int x, int y)
+//{
+//	return x - y;
+//}
+//
+//int Mul(int x, int y)
+//{
+//	return x * y;
+//}
+//
+//int Div(int x, int y)
+//{
+//	return x / y;
+//}
+//
+//void calc(int (*pf)(int, int))
+//{
+//	int x = 0;
+//	int y = 0;
+//	int ret = 0;
+//	printf("请输入两个操作数:>");
+//	scanf("%d %d", &x, &y);
+//	ret = pf(x, y);
+//	printf("%d\n", ret);
+//}
+//
+//int main()
+//{
+//	int input = 0;
+//	
+//	do 
+//	{
+//		menu();
+//		printf("请选择:>");
+//		scanf("%d", &input);
+//		switch (input)
+//		{
+//		case 1:
+//			calc(Add);
+//			break;
+//		case 2:
+//			calc(Sub);
+//			break;
+//		case 3:
+//			calc(Mul);
+//			break;
+//		case 4:
+//			calc(Div);
+//			break;
+//		case 0:
+//			printf("退出计算器\n");
+//			break;
+//		default:
+//			printf("选择错误\n");
+//			break;
+//		}
+//	} while (input);
+//	return 0;
+//}
