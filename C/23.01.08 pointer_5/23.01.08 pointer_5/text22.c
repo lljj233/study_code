d99e5ab874ceeb40ec78249169560f79a526e238
LLJJ233 <963543574@qq.com>
2023-01-11 17:16:52 +0800

    指针笔试题

--- C/23.01.08 pointer_5/23.01.08 pointer_5/text22.c
+++ C/23.01.08 pointer_5/23.01.08 pointer_5/text22.c
@@ -126,3 +126,62 @@
 //	printf("%d\n", sizeof(a[3]));//如果数组存在第四行。那么a[3]是第四行的数组名，为16
 //	return 0;
 //}
+
+
+
+
+//指针笔试题
+
+//int main()
+//{
+//	int a[5] = { 1,2,3,4,5 };
+//	int* ptr = (int *)(&a+1);
+//	printf("%d %d", *(a + 1), *(ptr - 1));
+//	return 0;
+//}
+
+//struct Test
+//{
+//	int Num;
+//	char* pcName;
+//	short sDate;
+//	char cha[2];
+//	short sBa[4];
+//}*p;
+////假设p 的值为0x100000。 如下表表达式的值分别为多少？
+////已知，结构体Test类型的变量大小是20个字节
+//int main()
+//{
+//  p = (struct Test*)0x1000000;
+//	printf("%p\n", p + 0x1);//0x00100014
+//	printf("%p\n", (unsigned long)p + 0x1);//0x00100001
+//	printf("%p\n", (unsigned int*)p + 0x1);//0x00100004
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a[4] = { 1, 2, 3, 4 };
+//	int* ptr1 = (int*)(&a + 1);
+//	int* ptr2 = (int*)((int)a + 1);
+//	printf("%x,%x", ptr1[-1], *ptr2);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a[3][2] = { (0, 1), (2, 3), (4, 5) };
+//	int* p;
+//	p = a[0];
+//	printf("%d", p[0]);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a[5][5];
+//	int(*p)[4];
+//	p = a;
+//	printf("%p,%d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);
+//	return 0;
+//}
