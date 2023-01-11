b4e32189146d81b399d58c5f56c81209ff78a1ea
LLJJ233 <963543574@qq.com>
2023-01-11 17:25:17 +0800

    指针笔试题

--- C/23.01.08 pointer_5/23.01.08 pointer_5/text22.c
+++ C/23.01.08 pointer_5/23.01.08 pointer_5/text22.c
@@ -185,3 +185,24 @@
 //	printf("%p,%d\n", &p[4][2] - &a[4][2], &p[4][2] - &a[4][2]);
 //	return 0;
 //}
+
+//int main()
+//{
+//	char* a[] = { "work","at","alibaba" };
+//	char** pa = a;
+//	pa++;
+//	printf("%s\n", *pa);
+//	return 0;
+//}
+
+//int main()
+//{
+//	char* c[] = { "ENTER","NEW","POINT","FIRST" };
+//	char** cp[] = { c + 3,c + 2,c + 1,c };
+//	char*** cpp = cp;
+//	printf("%s\n", **++cpp);
+//	printf("%s\n", *-- * ++cpp + 3);
+//	printf("%s\n", *cpp[-2] + 3);
+//	printf("%s\n", cpp[-1][-1] + 1);
+//	return 0;
+//}
