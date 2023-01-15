13963b04d21e446121a40ba5e0f2844483944417
LLJJ233 <963543574@qq.com>
2023-01-15 11:31:30 +0800

    strcat的使用及注意事项

--- C/23.01.12 character function and String function_1/23.01.12 character function and String function_1/text23.c
+++ C/23.01.12 character function and String function_1/23.01.12 character function and String function_1/text23.c
@@ -102,3 +102,18 @@
 //	printf("%s\n", arr1);
 //	return 0;
 //}
+
+
+
+//strcat - 在一个字符串后加上字符串
+//源字符串必须以 '\0' 结束。
+//目标空间必须有足够的大，能容纳下源字符串的内容。
+//目标空间必须可修改。
+//int main()
+//{
+//	char arr1[20] = "hello";
+//	char arr2[] = " world";
+//	strcat(arr1, arr2);
+//	printf("%s\n", arr1);
+//	return 0;
+//}
