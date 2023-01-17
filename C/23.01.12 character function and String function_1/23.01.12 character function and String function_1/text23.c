b446a64cb044a108c839210262a4039fbce1199d
LLJJ233 <963543574@qq.com>
2023-01-17 21:15:06 +0800

    strtok的使用及注意事项

--- C/23.01.12 character function and String function_1/23.01.12 character function and String function_1/text23.c
+++ C/23.01.12 character function and String function_1/23.01.12 character function and String function_1/text23.c
@@ -288,3 +288,36 @@
 //	}
 //	return 0;
 //}
+
+
+
+//strtok
+//char * strtok ( char * str, const char * sep );
+//sep参数是个字符串，定义了用作分隔符的字符集合
+//第一个参数指定一个字符串，它包含了0个或者多个由sep字符串中一个或者多个分隔符分割的标记。
+//strtok函数找到str中的下一个标记，并将其用 \0 结尾，返回一个指向这个标记的指针。
+//（注：strtok函数会改变被操作的字符串，所以在使用strtok函数切分的字符串一般都是临时拷贝的内容并且可修改。）
+//strtok函数的第一个参数不为 NULL ，函数将找到str中第一个标记，strtok函数将保存它在字符串中的位置。
+//strtok函数的第一个参数为 NULL ，函数将在同一个字符串中被保存的位置开始，查找下一个标记。
+//如果字符串中不存在更多的标记，则返回 NULL 指针。
+//int main()
+//{
+//	char arr[] = "963543574@qq.com";
+//	//963543574 qq com
+//	char* p = "@.";//strtok会把数组中的@和.改成\0
+//	char buf[20] = { 0 };
+//	strcpy(buf, arr);
+//	char* ret = NULL;
+//	for (ret = strtok(buf, p); ret != NULL; ret = strtok(NULL,p))
+//	{
+//		printf("%s\n", ret);
+//	}
+//	/*char* ret = strtok(buf, p);
+//	printf("%s\n", ret);
+//	ret = strtok(NULL, p);
+//	printf("%s\n", ret);
+//	ret = strtok(NULL, p);
+//	printf("%s\n", ret);*/
+//
+//	return 0;
+//}
