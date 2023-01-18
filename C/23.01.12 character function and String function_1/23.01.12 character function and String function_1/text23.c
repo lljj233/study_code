67f229b76407663069d09202a6fe3a53a34c3427
LLJJ233 <963543574@qq.com>
2023-01-18 14:51:00 +0800

    模拟实现memcpy

--- C/23.01.12 character function and String function_1/23.01.12 character function and String function_1/text23.c
+++ C/23.01.12 character function and String function_1/23.01.12 character function and String function_1/text23.c
@@ -462,3 +462,29 @@
 //	memcpy(arr2, arr1 + 3, 20);
 //	return 0;
 //}
+
+
+
+//Ä£ÄâÊµÏÖmemcpy
+//void* my_memcpy(void* dest, const void* src, size_t num)
+//{
+//	void* ret = dest;
+//	assert(dest && src);
+//	while (num--)
+//	{
+//		*(char*)dest = *(char*)src;
+//		/*dest = (char*)dest + 1;
+//		src = (char*)src + 1;*/
+//		++(char*)dest;
+//		++(char*)src;
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	int arr2[10] = { 0 };
+//	my_memcpy(arr2, arr1 + 3, 20);
+//	return 0;
+//}
+
