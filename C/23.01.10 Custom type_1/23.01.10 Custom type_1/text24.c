a9500fee71d4ab38402a151987f3b928492098c2
LLJJ233 <963543574@qq.com>
2023-01-25 22:28:26 +0800

    结构体的初始化

--- C/23.01.10 Custom type_1/23.01.10 Custom type_1/text24.c
+++ C/23.01.10 Custom type_1/23.01.10 Custom type_1/text24.c
@@ -40,3 +40,34 @@
 //	struct Node* next;
 //};
 
+
+
+//结构体的初始化
+//struct Point
+//{
+//	int x;
+//	int y;
+//}p1 = { 10,20 };
+//struct Point p2 = { 20,30 };
+//
+//struct s
+//{
+//	int num;
+//	char ch;
+//	struct Point p;
+//	float d;
+//};
+//
+//int main()
+//{
+//	struct Point p3 = { 40.49 };
+//
+//	struct s s1 = { 38,'d',{38,58},3.14f };
+//	struct s s2 = { .p = {4,5}, .ch = 'o', .num = 49, .d = 3.39f };//乱序初始化
+//
+//	printf("%d %c %d %f\n", s1.num, s1.ch, s1.p.x, s1.d);
+//	printf("%d %c %d %f\n", s2.num, s2.ch, s2.p.x, s2.d);
+//
+//	return 0;
+//}
+
