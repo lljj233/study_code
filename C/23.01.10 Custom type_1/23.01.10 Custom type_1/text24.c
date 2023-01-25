8ba6d044718017f6b25d2a9b305fed8115257406
LLJJ233 <963543574@qq.com>
2023-01-25 22:58:11 +0800

    结构体内存对齐 ( 计算结构体大小)

--- C/23.01.10 Custom type_1/23.01.10 Custom type_1/text24.c
+++ C/23.01.10 Custom type_1/23.01.10 Custom type_1/text24.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
+#include<stddef.h>
 
 
 //结构体
@@ -68,6 +69,40 @@
 //	printf("%d %c %d %f\n", s1.num, s1.ch, s1.p.x, s1.d);
 //	printf("%d %c %d %f\n", s2.num, s2.ch, s2.p.x, s2.d);
 //
+//	return 0;
+//}
+
+
+
+//结构体内存对齐 - 计算结构体大小
+//1. 第一个成员在与结构体变量偏移量为0的地址处。
+//2. 其他成员变量要对齐到某个数字（对齐数）的整数倍的地址处。
+//对齐数 = 编译器默认的一个对齐数 与 该成员大小的较小值。(VS中默认的值为8)
+//3. 结构体总大小为最大对齐数（每个成员变量都有一个对齐数）的整数倍。
+//4. 如果嵌套了结构体的情况，嵌套的结构体对齐到自己的最大对齐数的整数倍处，结构体的整体大小就是所有最大对齐数（含嵌套结构体的对齐数）的整数倍。
+//struct S1
+//{
+//	char c1;
+//	int i;
+//	char c2;
+//};
+//
+//struct S2
+//{
+//	char c1;
+//	char c2;
+//	int i;
+//};
+//
+//int main()
+//{
+//	printf("%d\n", sizeof(struct S1));//12
+//	printf("%d\n", sizeof(struct S2));//8
+//
+//	//判断结构体成员的偏移量
+//	/*printf("%d\n", offsetof(struct S1, c1));
+//	printf("%d\n", offsetof(struct S1, i));
+//	printf("%d\n", offsetof(struct S1, c2));*/
 //	return 0;
 //}
 
