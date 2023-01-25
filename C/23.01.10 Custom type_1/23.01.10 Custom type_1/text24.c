4d585624c647804dea9f8861a0be8263ce2f404a
LLJJ233 <963543574@qq.com>
2023-01-25 22:08:59 +0800

    匿名结构体

---
+++ C/23.01.10 Custom type_1/23.01.10 Custom type_1/text24.c
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+
+//结构体
+
+//匿名结构体 - 直接在结构体后创建变量
+//struct
+//{
+//	char name[20];
+//	int age;
+//}x;
+
+//struct
+//{
+//	int a;
+//	char b;
+//	double c;
+//}x;
+//struct
+//{
+//	int a;
+//	char b;
+//	double c;
+//}* p;
+//int main()
+//{
+//	p = &x;
+//	//编译器会把上面的两个声明当成完全不同的两个类型。
+//	//所以是非法的。
+//	return 0;
+//}
+
