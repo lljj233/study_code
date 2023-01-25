5168f1192c874cd0164a416496abe5cd387d2ba6
LLJJ233 <963543574@qq.com>
2023-01-25 21:23:12 +0800

    memmove的使用及其模拟实现

--- C/23.01.12 character function and String function_1/23.01.12 character function and String function_1/text23.c
+++ C/23.01.12 character function and String function_1/23.01.12 character function and String function_1/text23.c
@@ -488,3 +488,45 @@
 //	return 0;
 //}
 
+
+
+//memmove - 实现自我拷贝
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	memmove(arr1, arr1 + 2, 20);
+//	return 0;
+//}
+
+//memmove的模拟实现
+//void* my_memmove(void* dest, const void* src, size_t num)
+//{
+//	assert(dest && src);
+//	void* ret = dest;
+//	if (dest < src)
+//	{
+//		//从前向后拷贝
+//		while (num--)
+//		{
+//			*(char*)dest = *(char*)src;
+//			dest = (char*)dest + 1;
+//			src = (char*)src + 1;
+//		}
+//	}
+//	else
+//	{
+//		//从后向前拷贝
+//		while (num--)
+//		{
+//			*((char*)dest + num) = *((char*)src + num);
+//		}
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	int arr1[] = { 1,2,3,4,5,6,7,8,9,10 };
+//	my_memmove(arr1, arr1 + 2, 20);
+//	return 0;
+//}
+
