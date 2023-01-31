d34a7417e655c219990eca09acdbe555e8138126
LLJJ233 <963543574@qq.com>
2023-01-31 22:36:03 +0800

    malloc函数和free函数的使用

---
+++ C/23.01.13 Dynamic Memory_1/23.01.13 Dynamic Memory_1/text25.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+
+//malloc
+//如果开辟成功，则返回一个指向开辟好空间的指针。
+//如果开辟失败，则返回一个NULL指针，因此malloc的返回值一定要做检查。
+//返回值的类型是 void* ，所以malloc函数并不知道开辟空间的类型，具体在使用的时候使用者自己来决定。
+//如果参数 size 为0，malloc的行为是标准是未定义的，取决于编译器。
+int main()
+{
+	int* p = (int*)malloc(40);
+	if (p == NULL)
+	{
+		printf("%s\n", strerror(errno));
+		return 1;
+	}
+	//存放1-10
+	int i = 0;
+	for (i = 0; i < 10; i++)
+	{
+		*(p + i) = i + 1;
+	}
+	//打印
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d ", *(p + i));
+	}
+	//释放申请的内存
+	free(p);
+	//free函数用来释放动态开辟的内存。
+	//如果参数 ptr 指向的空间不是动态开辟的，那free函数的行为是未定义的。
+	//如果参数 ptr 是NULL指针，则函数什么事都不做。
+	p = NULL;
+	return 0;
+}
+//malloc和free都声明在 stdlib.h 头文件中。
+
