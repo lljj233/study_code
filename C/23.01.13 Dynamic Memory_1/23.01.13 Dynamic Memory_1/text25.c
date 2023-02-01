8bb55d6a09132d4c202f4af24c4a10b349a11ccb
LLJJ233 <963543574@qq.com>
2023-02-01 16:33:58 +0800

    常见的动态内存错误

--- C/23.01.13 Dynamic Memory_1/23.01.13 Dynamic Memory_1/text25.c
+++ C/23.01.13 Dynamic Memory_1/23.01.13 Dynamic Memory_1/text25.c
@@ -99,3 +99,102 @@
 //	return 0;
 //}
 
+
+
+//常见的动态内存错误
+
+//1.对NULL指针的解引用操作
+//int main()
+//{
+//	int* p = (int*)malloc(100);
+//	//这里要判断malloc的返回值
+//	int i = 0;
+//	for (i = 0; i < 20; i++)
+//	{
+//		*(p + i) = 0;
+//	}
+//	return 0;
+//}
+
+//2.对动态开辟空间的越界访问
+//int main()
+//{
+//	int* p = (int*)malloc(100);
+//	//25个整型
+//	if (p == NULL)
+//		return 1;
+//	int i = 0;
+//	for (i = 0; i < 100; i++)
+//	{
+//		*(p + i) = 0;//越界访问
+//	}
+//	free(p);
+//	return 0;
+//}
+
+//3.对非动态开辟内存使用free释放
+//int main()
+//{
+//	int a = 10;//栈区
+//	int* p = &a;
+//	free(p);
+//	p = NULL;
+//	return 0;
+//}
+
+//4.使用free释放一块动态开辟内存的一部分
+//int main()
+//{
+//	int* p = (int*)malloc(100);
+//	if (p == NULL)
+//		return 1;
+//	int i = 0;
+//	for (i = 0; i < 25; i++)
+//	{
+//		*p = i;//越界访问
+//		p++;
+//	}
+//	free(p);//此时p已经不在指向起始位置的地址
+//	p = NULL;
+//	return 0;
+//}
+
+//5.对同一块动态内存多次释放
+//int main()
+//{
+//	int* p = (int*)malloc(100);
+//	if (p == NULL)
+//		return 1;
+//	//使用
+//	//释放
+//	free(p);
+//	//正确：加上p = NULL;
+//	//...
+//
+//	free(p);//多次释放
+//	return 0;
+//}
+
+//6.动态开辟内存忘记释放（内存泄漏）
+//函数内部进行了malloc操作，返回了malloc开辟的空间的起始地址
+//使用完后记得释放
+//int* test()
+//{
+//	int* p = (int*)malloc(100);
+//	if (p == NULL)
+//		return 1;
+//	//使用
+//	//...
+//	//要记得free(p);p = NULL;
+//	//或者return p;
+//}
+//int main()
+//{
+//	//若return p则用一个指针接收后释放
+//	int* ptr = test();
+//	free(ptr);
+//	ptr = NULL;
+//	//...
+//	return 0;
+//}
+
