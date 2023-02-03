8a5b570a2fa4361b4c1046edab91299147c1eb26
LLJJ233 <963543574@qq.com>
2023-02-03 15:16:15 +0800

    通讯录2

---
+++ C/通讯录2/通讯录2/contact.c
@@ -0,0 +1,192 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"contact.h"
+
+
+void Init_Contact(Contact* pc)
+{
+	assert(pc);
+	pc->size = 0;
+	Peoinfo* ptr = (Peoinfo*)calloc(DEFAULT_SIZE, sizeof(Peoinfo));
+	if (ptr == NULL)
+	{
+		perror("InitContact::calloc");
+		return;
+	}
+	pc->data = ptr;
+	pc->cacpcity = DEFAULT_SIZE;
+}
+
+void Destroy_Contact(Contact* pc)
+{
+	free(pc->data);
+	pc->data = NULL;
+	pc->cacpcity = 0;
+	pc->size = 0;
+	pc = NULL;
+}
+
+void check_capacity(Contact* pc)
+{
+	if (pc->size == pc->cacpcity)
+	{
+		//增容
+		Peoinfo* ptr = (Peoinfo*)realloc(pc->data, (pc->cacpcity + 2) * sizeof(Peoinfo));
+		if (ptr == NULL)
+		{
+			printf("增加容量失败\n");
+			perror("check_capacity::realloc");
+			return;
+		}
+		pc->data = ptr;
+		pc->cacpcity += INC_SIZE;
+		printf("增容成功\n");
+	}
+}
+
+void Add_Contact(Contact* pc)
+{
+	assert(pc);
+	check_capacity(pc);
+	if (pc->size == pc->cacpcity)
+	{
+		printf("通讯录已满，无法添加");
+		return;
+	}
+	//增加一个联系人的信息
+	printf("请输入姓名：");
+	scanf("%s", pc->data[pc->size].name);
+	printf("请输入年龄：");
+	scanf("%d", &pc->data[pc->size].age);
+	printf("请输入性别：");
+	scanf("%s", pc->data[pc->size].sex);
+	printf("请输入住址：");
+	scanf("%s", pc->data[pc->size].address);
+	printf("请输入电话号码：");
+	scanf("%s", pc->data[pc->size].telephone_number);
+
+	pc->size++;
+
+	printf("增添成功\n");
+}
+
+void Show_Contact(const Contact* pc)
+{
+	assert(pc);
+	printf("-----------------------------\n");
+	int i = 0;
+	for (i = 0; i < pc->size; i++)
+	{
+		printf("姓名：%s\n", pc->data[i].name);
+		printf("年龄：%d\n", pc->data[i].age);
+		printf("性别：%s\n", pc->data[i].sex);
+		printf("住址：%s\n", pc->data[i].address);
+		printf("电话号码：%s\n", pc->data[i].telephone_number);
+		printf("-----------------------------\n");
+	}
+}
+
+int Find_By_Name(const Contact* pc, char name[])
+{
+	int i = 0;
+	for (i = 0; i < pc->size; i++)
+	{
+		if (strcmp(pc->data[i].name, name) == 0)
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+void Del_Contact(Contact* pc)
+{
+	assert(pc);
+	char name[NAME_MAX] = { 0 };
+	if (pc->size == 0)
+	{
+		printf("通讯录为空，无法删除\n");
+		return;
+	}
+	//删除一个联系人的信息
+	printf("请输入要删除联系人的姓名：");
+	scanf("%s", name);
+	int ret = Find_By_Name(pc, name);
+	if (ret == -1)
+	{
+		printf("该联系人不存在\n");
+		return;
+	}
+	int i = 0;
+	//删除
+	for (i = ret; i < pc->size - 1; i++)
+	{
+		pc->data[i] = pc->data[i + 1];
+	}
+	pc->size--;
+	printf("删除成功\n");
+}
+
+void Search_Contact(const Contact* pc)
+{
+	assert(pc);
+	char name[NAME_MAX] = { 0 };
+	printf("请输入要查找的联系人名字：");
+	scanf("%s", name);
+	int pos = Find_By_Name(pc, name);
+	if (pos == -1)
+	{
+		printf("要查找的联系人不存在\n");
+		return;
+	}
+	printf("姓名：%s\n", pc->data[pos].name);
+	printf("年龄：%d\n", pc->data[pos].age);
+	printf("性别：%s\n", pc->data[pos].sex);
+	printf("住址：%s\n", pc->data[pos].address);
+	printf("电话号码：%s\n", pc->data[pos].telephone_number);
+	printf("-----------------------------\n");
+}
+
+void Modify_Contact(Contact* pc)
+{
+	assert(pc);
+	char name[NAME_MAX] = { 0 };
+	printf("请输入要修改的联系人名字：");
+	scanf("%s", name);
+	int pos = Find_By_Name(pc, name);
+	if (pos == -1)
+	{
+		printf("要查找的联系人不存在\n");
+		return;
+	}
+	printf("请输入姓名：");
+	scanf("%s", pc->data[pos].name);
+	printf("请输入年龄：");
+	scanf("%d", &pc->data[pos].age);
+	printf("请输入性别：");
+	scanf("%s", pc->data[pos].sex);
+	printf("请输入住址：");
+	scanf("%s", pc->data[pos].address);
+	printf("请输入电话号码：");
+	scanf("%s", pc->data[pos].telephone_number);
+
+	printf("修改完成\n");
+}
+
+void Clear_Contact(Contact* pc)
+{
+	assert(pc);
+	Init_Contact(pc);
+	printf("已清空所有联系人\n");
+}
+
+int cmp_stu_by_name(const void* e1, const void* e2)
+{
+	return strcmp(((Contact*)e1)->data->name, ((Contact*)e2)->data->name);
+}
+
+void Sort_Contact(Contact* pc)
+{
+	qsort(pc->data, pc->size, sizeof(pc->data[0]), cmp_stu_by_name);
+	printf("排序成功\n");
+}
+
---
+++ C/通讯录2/通讯录2/contact.h
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+#include<stdio.h>
+#include<string.h>
+#include<assert.h>
+#include<stdlib.h>
+
+#define MAX 1000
+#define NAME_MAX 20
+#define SEX_MAX 5
+#define ADDRESS_MAX 30
+#define TELE_MAX 12
+
+#define DEFAULT_SIZE 3//默认大小为3
+#define INC_SIZE 2//每次增长2
+
+
+typedef struct People_Information
+{
+	char name[NAME_MAX];
+	int age;
+	char sex[SEX_MAX];
+	char address[ADDRESS_MAX];
+	char telephone_number[TELE_MAX];
+}Peoinfo;
+
+typedef struct Contact
+{
+	Peoinfo* data;//指向存放联系人的空间
+	int size;//通讯录目前已存放联系人的数量
+	int cacpcity;//当前通讯录的最大容量
+}Contact;
+
+void Init_Contact(Contact* pc);//初始化通讯录
+void Destroy_Contact(Contact* pc);//销毁化通讯录
+void Add_Contact(Contact* pc);//增加联系人
+void Del_Contact(Contact* pc);//删除联系人
+void Search_Contact(const Contact* pc);//查找联系人
+void Modify_Contact(Contact* pc);//修改联系人
+void Show_Contact(const Contact* pc);//显示联系人
+void Clear_Contact(Contact* pc);//清空联系人
+void Sort_Contact(Contact* pc);//排序联系人
+
