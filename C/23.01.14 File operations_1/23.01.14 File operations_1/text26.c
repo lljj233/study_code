32cc091593687bc1367bd86e83dbb04ea459d2cd
LLJJ233 <963543574@qq.com>
2023-02-05 21:26:53 +0800

    打开和关闭文件

---
+++ C/23.01.14 File operations_1/23.01.14 File operations_1/text26.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+
+//打开和关闭文件
+//int main()
+//{
+//	//打开文件
+//	
+//	//FILE* pf = fopen("C:\\code\\test.txt", "r");
+//	//  C:\\code\\test.txt - 绝对路径
+//
+//	FILE* pf = fopen("test.txt", "r");//打开的文件与当前项目在同一目录中
+//	//  test.txt - 相对路径
+//
+//	//FILE* pf = fopen("../test.txt", "r");
+//	//  ../ - 上一级目录
+//
+//	if (pf == NULL)
+//	{
+//		perror("fopen");
+//		return 1;
+//	}
+//	else
+//	{
+//		printf("打开文件成功\n");
+//	}
+//
+//	//读文件
+//	//操作文件
+//
+//	//关闭文件
+//	fclose(pf);
+//	pf = NULL;
+//	return 0;
+//}
+
