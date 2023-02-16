dd762426123843999d673aaaf801a9478f6f841a
LLJJ233 <963543574@qq.com>
2023-02-16 16:44:18 +0800

    python基本的数字运算

---
+++ OJ/newcoder/text.py
@@ -0,0 +1,16 @@
+#python基本的数字运算
+a=5
+b=2
+c=-8
+d=3.14159
+print(a//b)# 取整数部分
+print(a**b)# a的b次方
+print(pow(a,b))# a的b次方
+print(abs(c))# c的绝对值
+print(round(d,3))# d保留三位小数
+print(round(d))# d保留整数（保留0位小数）
+print(max(a,b,c,d,))# 这几个数里的最大值
+print(min(a,b,c,d,))# 这几个数里的最小值
+print(int(d))# 取d的整数部分
+print(float(d))# d不是整数就为该数，d是整数就为该数.0
+
