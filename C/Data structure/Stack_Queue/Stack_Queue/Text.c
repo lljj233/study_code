ddc0a8305bb1107b548b63a9fa884ed57772880f
LLJJ233 <963543574@qq.com>
2023-04-08 16:08:01 +0800

    队列的创建及其基本操作

--- C/Data structure/Stack_Queue/Stack_Queue/Text.c
+++ C/Data structure/Stack_Queue/Stack_Queue/Text.c
@@ -1,26 +1,48 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include"stack.h"
+#include"queue.h"
+
+//int main()
+//{
+//	ST st;
+//	STInit(&st);
+//	STPush(&st, 1);
+//	STPush(&st, 2);
+//	STPush(&st, 3);
+//	printf("%d ", STTop(&st));
+//	STPop(&st);
+//	STPush(&st, 4);
+//	STPush(&st, 5);
+//	
+//	while (!STEmpty(&st))
+//	{
+//		printf("%d ", STTop(&st));
+//		STPop(&st);
+//	}
+//
+//	STDestroy(&st);
+//
+//	return 0;
+//}
 
 int main()
 {
-	ST st;
-	STInit(&st);
-	STPush(&st, 1);
-	STPush(&st, 2);
-	STPush(&st, 3);
-	printf("%d ", STTop(&st));
-	STPop(&st);
-	STPush(&st, 4);
-	STPush(&st, 5);
-	
-	while (!STEmpty(&st))
+	Queue q;
+	QueueInit(&q);
+	QueuePush(&q, 1);
+	QueuePush(&q, 2);
+	QueuePush(&q, 3);
+	QueuePush(&q, 4);
+	QueuePush(&q, 5);
+
+	while (!QueueEmpty(&q))
 	{
-		printf("%d ", STTop(&st));
-		STPop(&st);
+		printf("%d ", QueueFront(&q));
+		QueuePop(&q);
 	}
+	printf("\n");
 
-	STDestroy(&st);
-
+	QueueDestroy(&q);
 	return 0;
 }
 
