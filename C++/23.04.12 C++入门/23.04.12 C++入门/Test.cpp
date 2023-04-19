8cc754ad75b512409c3d7c4aa498992aa5a9ea14
LLJJ233 <963543574@qq.com>
2023-04-19 20:19:07 +0800

    缺省参数

--- C++/23.04.12 C++入门/23.04.12 C++入门/Test.cpp
+++ C++/23.04.12 C++入门/23.04.12 C++入门/Test.cpp
@@ -148,3 +148,59 @@
 //}
 
 
+
+//缺省参数
+
+//#include<iostream>
+//using namespace std;
+
+//void Func(int a = 0)
+//{
+//	cout << a << endl;
+//}
+//int main()
+//{
+//	Func();    //没有传参时，使用参数的默认值
+//	Func(10);  //传参时，使用指定的实参
+//	return 0;
+//}
+
+//全缺省参数(所有的参数都给了缺省值)
+//void Func(int a = 10, int b = 20, int c = 30)
+//{
+//	cout << "a = " << a << endl;
+//	cout << "b = " << b << endl;
+//	cout << "c = " << c << endl;
+//}
+//int main()
+//{
+//	Func();
+//	Func(1);
+//	Func(1, 2);
+//	Func(1, 2, 3);
+//
+//	return 0;
+//}
+
+//半缺省参数(部分的参数都给了缺省值)
+//从右往左缺省
+//void Func(int a, int b = 10, int c = 20)
+//{
+//	cout << "a = " << a << endl;
+//	cout << "b = " << b << endl;
+//	cout << "c = " << c << endl;
+//}
+//int main()
+//{
+//	//Func();  //error
+//	Func(1);
+//	Func(1, 2);
+//	Func(1, 2, 3);
+//	return 0;
+//}
+
+//注意：
+//1. 半缺省参数必须从右往左依次来给出，不能间隔着给
+//2. 缺省参数不能在函数声明和定义中同时出现(声明给，定义不给)
+//3. 缺省值必须是常量或者全局变量
+//4. C语言不支持（编译器不支持）
