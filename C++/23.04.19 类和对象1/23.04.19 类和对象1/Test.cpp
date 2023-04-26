97a5aeafb21ed0daf52e11d9e5efc508d18ffd0d
LLJJ233 <963543574@qq.com>
2023-04-26 22:00:00 +0800

    类的访问限定符

--- C++/23.04.19 类和对象1/23.04.19 类和对象1/Test.cpp
+++ C++/23.04.19 类和对象1/23.04.19 类和对象1/Test.cpp
@@ -45,3 +45,63 @@
 //	return 0;
 //}
 
+
+
+//类的访问限定符
+//C++实现封装的方式：用类将对象的属性与方法结合在一块，让对象更加完善，通过访问权限选择性的将其接口提供给外部的用户使用
+//访问限定符：
+//1. public     公有
+//2. protected  保护
+//3. private    私有
+
+//访问限定符说明
+//1. public修饰的成员在类外可以直接被访问
+//2. protected和private修饰的成员在类外不能直接被访问(此处protected和private是类似的)
+//3. 访问权限作用域从该访问限定符出现的位置开始直到下一个访问限定符出现时为止
+//4. 如果后面没有访问限定符，作用域就到 } 即类结束。
+//5. class的默认访问权限为private，struct为public(因为struct要兼容C)
+
+//#include<iostream>
+//struct Stack
+//{
+//public:
+//	void Init(int defaultCapacity = 4)
+//	{
+//		a = (int*)malloc(sizeof(int) * capacity);
+//		if (nullptr == a)
+//		{
+//			perror("malloc申请空间失败");
+//			return;
+//		}
+//		capacity = capacity;
+//		top = 0;
+//	}
+//	void Push(int x)
+//	{
+//		//扩容
+//		a[top--] = x;
+//	}
+//	void Destroy()
+//	{
+//		free(a);
+//		a = nullptr;
+//		top = capacity;
+//	}
+//	
+//private:
+//	int* a;
+//	int top;
+//	int capacity;
+//};
+//int main()
+//{
+//	struct Stack st1;
+//	st1.Init(10);
+//	
+//	Stack st2;
+//	st2.Init();
+//	st2.Push(1);
+//	st2.Destroy();
+//	return 0;
+//}
+
