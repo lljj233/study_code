3540818c51fe8e60d061ef247527f02761daadd9
LLJJ233 <963543574@qq.com>
2023-07-01 20:34:54 +0800

    函数模板实例化

--- C++/23.05.12 模板/23.05.12 模板/test.cpp
+++ C++/23.05.12 模板/23.05.12 模板/test.cpp
@@ -42,3 +42,32 @@ using namespace std;
 //	return 0;
 //}
 
+
+
+//函数模板实例化: 1.隐式实例化 2.显式实例化
+//template<class T>
+//T Add(const T& left, const T& right)
+//{
+//	return left + right;
+//}
+//int main()
+//{
+//	int a1 = 10, a2 = 20;
+//	double d1 = 10.0, d2 = 20.0;
+//	cout << Add(a1, a2) << endl;
+//	cout << Add(d1, d2) << endl;
+//
+//	//cout << Add(a1, d1) << endl;
+//	//当编译器看到该实例化时，需要推演其实参类型通过实参a1将T推演为int，通过实参d1将T推演为double类型
+//	//但模板参数列表中只有一个T，编译器无法确定此处到底该将T确定为int 或者 double类型而报错
+//	//处理方式：
+//	//1. 用户自己来强制转化
+//	cout << Add(a1, (int)d1) << endl;
+//	cout << Add((double)a1, d1) << endl;
+//	//2. 使用显式实例化
+//	cout << Add<int>(a1, d1) << endl;
+//	cout << Add<double>(a1, d1) << endl;
+//
+//	return 0;
+//}
+
