0690c6b5d34ae623d6d3e40c5e3b253302fbd1c9
lljj233 <963543574@qq.com>
2024-01-22 16:04:38 +0800

    单值二叉树

---
+++ OJ/leetcode/965 单值二叉树.c
@@ -0,0 +1,20 @@
+//https://leetcode.cn/problems/univalued-binary-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool isUnivalTree(struct TreeNode* root) {
+    if(root==NULL)
+        return true;
+    if(root->left && root->left->val != root->val)
+        return false;
+    if(root->right && root->right->val != root->val)
+        return false;
+    return isUnivalTree(root->left) && isUnivalTree(root->right);
+}
+
