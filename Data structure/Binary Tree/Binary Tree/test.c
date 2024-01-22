49a9203578bfa41fc4e41ce1073e1055577d20b3
lljj233 <963543574@qq.com>
2024-01-22 14:36:15 +0800

    二叉树的三种遍历方式

---
+++ Data structure/Binary Tree/Binary Tree/test.c
@@ -0,0 +1,95 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#pragma once
+
+#include<stdio.h>
+#include<assert.h>
+#include<stdlib.h>
+
+typedef int BTDataType;
+typedef struct BinaryTreeNode
+{
+	BTDataType data;
+	struct BinaryTreeNode* left;
+	struct BinaryTreeNode* right;
+}BTNode;
+
+BTNode* BuyNode(BTDataType x)
+{
+	BTNode* node = (BTNode*)malloc(sizeof(BTNode));
+	if (node == NULL)
+	{
+		perror("malloc fail");
+		return;
+	}
+	node->data = x;
+	node->left = NULL;
+	node->right = NULL;
+	return node;
+}
+
+BTNode* CreateBinaryTree()
+{
+	BTNode* node1 = BuyNode(1);
+	BTNode* node2 = BuyNode(2);
+	BTNode* node3 = BuyNode(3);
+	BTNode* node4 = BuyNode(4);
+	BTNode* node5 = BuyNode(5);
+	BTNode* node6 = BuyNode(6);
+
+	node1->left = node2;
+	node1->right = node4;
+	node2->left = node3;
+	node4->left = node5;
+	node4->right = node6;
+
+	return node1;
+}
+
+void PreOrder(BTNode* root)//前序遍历
+{
+	if (root == NULL)
+	{
+		printf("NULL ");
+		return;
+	}
+	printf("%d ", root->data);
+	PreOrder(root->left);
+	PreOrder(root->right);
+
+}
+
+void InOrder(BTNode* root)//中序遍历
+{
+	if (root == NULL)
+	{
+		printf("NULL ");
+		return;
+	}
+	InOrder(root->left);
+	printf("%d ", root->data);
+	InOrder(root->right);
+}
+
+void PostOrder(BTNode* root)//后序遍历
+{
+	if (root == NULL)
+	{
+		printf("NULL ");
+		return;
+	}
+	PostOrder(root->left);
+	PostOrder(root->right);
+	printf("%d ", root->data);
+}
+
+int main()
+{
+	BTNode* root = CreateBinaryTree();
+	PreOrder(root);
+	printf("\n");
+	InOrder(root);
+	printf("\n");
+	PostOrder(root);
+	printf("\n");
+	return 0;
+}
